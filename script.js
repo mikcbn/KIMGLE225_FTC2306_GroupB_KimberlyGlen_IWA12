b37ea585c2042744ff828ba91ec8915cc2de1dae
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-28 09:06:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,27 @@ const STATUS_MAP = {
     }
 }
 
-// Edit below lines
+// Edit below line
 
-updateBookStatus('book1', 'overdue');
-updateBookStatus('book2', 'reserved');
-updateBookStatus('book3', 'shelf');
+const statusElements = document.getElementsByClassName("status");
+const reserveButtons = document.getElementsByClassName("reserve");
+const checkoutButtons = document.getElementsByClassName("checkout");
+const checkinButtons = document.getElementsByClassName("checkin");
+
+for (let i = 0; i < statusElements.length; i++) {
+  const statusText = statusElements[i].textContent.trim().toLowerCase();
+  const statusInfo = STATUS_MAP[statusText];
+
+  // Update status text color
+  statusElements[i].style.color = statusInfo.color;
+
+  // Set button states based on STATUS_MAP
+  reserveButtons[i].disabled = !statusInfo.canReserve;
+  checkoutButtons[i].disabled = !statusInfo.canCheckout;
+  checkinButtons[i].disabled = !statusInfo.canCheckIn;
+
+  // Make buttons grayscale
+  reserveButtons[i].style.color = "black";
+  checkoutButtons[i].style.color = "black";
+  checkinButtons[i].style.color = "black";
+}
