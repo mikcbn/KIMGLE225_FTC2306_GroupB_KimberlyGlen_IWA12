8488d57f6cf8e40a805367b2f69388ea269a024a
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-29 19:45:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,27 +27,50 @@ const STATUS_MAP = {
     }
 }
 
-// Edit below line
-
 const statusElements = document.getElementsByClassName("status");
 const reserveButtons = document.getElementsByClassName("reserve");
 const checkoutButtons = document.getElementsByClassName("checkout");
 const checkinButtons = document.getElementsByClassName("checkin");
 
 for (let i = 0; i < statusElements.length; i++) {
-  const statusText = statusElements[i].textContent.trim().toLowerCase();
-  const statusInfo = STATUS_MAP[statusText];
+    const statusText = statusElements[i].textContent.trim().toLowerCase();
+    const statusInfo = STATUS_MAP[statusText];
 
-  // Update status text color
-  statusElements[i].style.color = statusInfo.color;
+    // Update status text color
+    statusElements[i].style.color = statusInfo.color;
 
-  // Set button states based on STATUS_MAP
-  reserveButtons[i].disabled = !statusInfo.canReserve;
-  checkoutButtons[i].disabled = !statusInfo.canCheckout;
-  checkinButtons[i].disabled = !statusInfo.canCheckIn;
+    // Set button states based on STATUS_MAP
+    reserveButtons[i].disabled = !statusInfo.canReserve;
+    checkoutButtons[i].disabled = !statusInfo.canCheckout;
+    checkinButtons[i].disabled = !statusInfo.canCheckIn;
 
-  // Make buttons grayscale
-  reserveButtons[i].style.color = "black";
-  checkoutButtons[i].style.color = "black";
-  checkinButtons[i].style.color = "black";
+    // Make buttons grayscale if not selectable
+    if (!statusInfo.canReserve) {
+        reserveButtons[i].style.filter = "grayscale(100%)";
+    }
+    if (!statusInfo.canCheckout) {
+        checkoutButtons[i].style.filter = "grayscale(100%)";
+    }
+    if (!statusInfo.canCheckIn) {
+        checkinButtons[i].style.filter = "grayscale(100%)";
+        checkinButtons[i].style.color = "green"; // Change the text color of the "Check In" button to green
+    }
 }
+
+// Your new code below this comment line (for each book)
+
+// For book1
+const book1Status = document.querySelector('#book1 .status').textContent.trim().toLowerCase();
+const book1StatusInfo = STATUS_MAP[book1Status];
+document.querySelector('#book1 .status').style.color = book1StatusInfo.color;
+
+// For book2
+const book2Status = document.querySelector('#book2 .status').textContent.trim().toLowerCase();
+const book2StatusInfo = STATUS_MAP[book2Status];
+document.querySelector('#book2 .status').style.color = book2StatusInfo.color;
+
+// For book3
+const book3Status = document.querySelector('#book3 .status').textContent.trim().toLowerCase();
+const book3StatusInfo = STATUS_MAP[book3Status];
+document.querySelector('#book3 .status').style.color = book3StatusInfo.color;
+
